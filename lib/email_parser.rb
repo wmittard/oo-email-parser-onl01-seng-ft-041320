ec4da9092969feb056b1febc634578a373e4787a
William Mittard <billymittard@ymail.com>
2020-04-24 18:18:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,4 @@
-class EmailParser 
+class EmailAddressParser 
   attr_accessor :name, :csv_emails
   
   def initialize(csv_emails)
@@ -21,11 +21,11 @@ class EmailParser
 end 
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
+parser1 = EmailAddressParser.new(emails1)
 parser1.parse 
 
 emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
+parser2 = EmailAddressParserParser.new(emails2)
 parser2.parse
 
   
