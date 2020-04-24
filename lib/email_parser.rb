f5590c356d88c415676c1baccd88c3bb37311220
William Mittard <billymittard@ymail.com>
2020-04-24 17:43:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,7 @@
+class EmailParser
+  def intialize("")
+  end 
+end 
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
