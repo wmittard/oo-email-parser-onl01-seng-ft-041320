e8504b840c0a3ce285b20427cc25dfbc93b05545
William Mittard <billymittard@ymail.com>
2020-04-24 17:45:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,6 +2,7 @@ class EmailParser
   def intialize(CSV emails)
     @CSV_emails = CSV_emails
   end 
+  
 end 
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
