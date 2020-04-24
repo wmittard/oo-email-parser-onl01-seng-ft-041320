5b0639af72b5929b6aba2690f9fe9a06eabc6ae8
William Mittard <billymittard@ymail.com>
2020-04-24 18:15:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,16 +1,3 @@
-class EmailParser
 
-  attr_accessor :email
-  def  initialize(emails)
-    @email = emails
-  end
-
-  def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
-  end
-  
-  end
   
 
