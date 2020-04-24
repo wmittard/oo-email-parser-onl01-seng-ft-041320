411dca5317d2d907e351408811c03272518d219a
William Mittard <billymittard@ymail.com>
2020-04-24 17:58:07 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,7 +6,7 @@ class EmailParser
   end 
   def parse 
    email_array = @email.split (/[', ']/).uniq
-   email_array.reject! { |element| element.empty? }
+   email_array.reject! {|element| element.empty?}
    email_array
   end 
 end 
